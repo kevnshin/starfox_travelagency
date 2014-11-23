d8389153a9fbc53f69434561228a0faf7af0525a
Kevin Shin <kevnshin@gmail.com>
2014-11-22 17:38:19 -1000

    Finished Header and Nav for Desktop version

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function () {
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./sass/**/*.scss')
       .pipe(sass())
       .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch_styles', function () {
-  gulp.watch('./sass/**/.scss', ['styles']);
+  gulp.watch('./sass/**/*.scss', ['styles']);
   gulp.watch('*.html', notifyLiveReload);
   gulp.watch('css/*.css', notifyLiveReload);
 })
